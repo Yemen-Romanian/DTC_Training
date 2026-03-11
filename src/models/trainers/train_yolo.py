02b0627d2bf1fa4e850e01a506a41b10fdffe7a1
Yevhen Romaniak <zhenia97.romanticized@gmail.com>
2026-03-11 22:43:26 +0200

    Added validation for YOLO model

--- src/models/trainers/train_yolo.py
+++ src/models/trainers/train_yolo.py
@@ -5,4 +5,5 @@ model = YOLO("yolo11n.pt")
 results = model.train(data=r"Path To Yaml", 
                       epochs=20, single_cls=True, 
                       imgsz=640, classes=[0], batch=4)
+model.val()
 print("============ Training finished!=================")
