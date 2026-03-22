7572bf73da6b29baf0f7c69009e4aa2453d4e0bf
Yevhen Romaniak <romanyak.yevhen@gmail.com>
2026-03-22 20:50:36 +0200

    Modified SiamFC tracker for scale changes

--- src/models/trackers/siamfc.py
+++ src/models/trackers/siamfc.py
@@ -34,7 +34,7 @@ class SiamFCNet(nn.Module):
         b, c, h, w = search_features.size()
         _, _, hz, wz = examplar_features.size()
         
-        search_features = search_features.view(1, b * c, h, w)
+        search_features = search_features.reshape(1, b * c, h, w)
         examplar_features = examplar_features.view(b, c, hz, wz)
         
         score = F.conv2d(search_features, examplar_features, groups=b) # [1, b, 17, 17]
@@ -49,7 +49,9 @@ class TrackerSiamFC(TrackerBase):
     ROI_PAD_FACTOR = 2
 
     def __init__(self, model):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = model
+        self.model.to(self.device)
         self.model.eval()
         self.score_size = 17
         
@@ -57,6 +59,14 @@ class TrackerSiamFC(TrackerBase):
         self.window = np.outer(hann, hann)
         self.window /= self.window.sum()
         self.window_influence = 0.176
+
+        self.scale_step = 1.0375
+        self.scale_penalty = 0.9745
+        self.scale_lr = 0.59
+        self.scale_num = 3
+
+        self.scales = [self.scale_step ** i for i in range(-(self.scale_num // 2), self.scale_num // 2 + 1)]
+
 
     def _get_subwindow(self, image, pos, model_sz, original_sz, avg_chans):
         sz = original_sz
@@ -103,30 +113,53 @@ class TrackerSiamFC(TrackerBase):
         self.s_x = self.s_z * (self.ROI_SIZE / self.EXAMPLAR_SIZE)
         z_crop = self._get_subwindow(image, self.pos, self.EXAMPLAR_SIZE, self.s_z, image.mean(axis=(0, 1)))
         z_tensor = torch.from_numpy(z_crop).permute(2, 0, 1).float().unsqueeze(0) / 255.0
+        z_tensor = z_tensor.to(self.device)
         
         with torch.no_grad():
             self.examplar_features = self.model.extract_features(z_tensor)
 
     def track(self, image):
-        x_crop = self._get_subwindow(image, self.pos, self.ROI_SIZE, self.s_x, image.mean(axis=(0, 1)))
-        x_tensor = torch.from_numpy(x_crop).permute(2, 0, 1).float().unsqueeze(0) / 255.0
-        search_image_features = self.model.extract_features(x_tensor)
+        scales = [self.scale_step ** i for i in range(-(self.scale_num // 2), self.scale_num // 2 + 1)]
+
+        crops = []
+        for s in scales:
+            cur_s_x = self.s_x * s
+            crop = self._get_subwindow(image, self.pos, self.ROI_SIZE, cur_s_x, image.mean(axis=(0, 1)))
+            crops.append(crop)
+
+        x_batch = torch.from_numpy(np.stack(crops)).permute(0, 3, 1, 2).float().to(self.device) / 255.0
+        x_feat = self.model.extract_features(x_batch)
 
         with torch.no_grad():
-            score_map = self.model.compute_score(self.examplar_features, search_image_features).squeeze().cpu().numpy()
+            z_feat = self.examplar_features.repeat(self.scale_num, 1, 1, 1)
+            score_maps = self.model.compute_score(z_feat, x_feat).squeeze().cpu().numpy()
 
-        score_map -= score_map.min()
-        score_map /= (score_map.sum() + 1e-5)
-        score_map = (1 - self.window_influence) * score_map + self.window_influence * self.window
+        score_maps[0, :, :] *= self.scale_penalty
+        score_maps[2, :, :] *= self.scale_penalty
 
-        r_max, c_max = np.unravel_index(score_map.argmax(), score_map.shape)
+        scale_idx = np.argmax(np.amax(score_maps, axis=(1, 2)))
+        best_score_map = score_maps[scale_idx]
+
+        print("Confidence:", best_score_map.max())
+        best_score_map -= best_score_map.min()
+        best_score_map /= (best_score_map.sum() + 1e-5)
+        best_score_map = (1 - self.window_influence) * best_score_map + self.window_influence * self.window
+
+        chosen_scale = scales[scale_idx]
+        self.s_x = (1 - self.scale_lr) * self.s_x + self.scale_lr * (self.s_x * chosen_scale)
+
+        self.target_sz = (1 - self.scale_lr) * self.target_sz + self.scale_lr * (self.target_sz * chosen_scale)
+
+        r_max, c_max = np.unravel_index(best_score_map.argmax(), best_score_map.shape)
         disp_score = np.array([r_max - 8, c_max - 8])
-        disp_real = disp_score * 8 * (self.s_x / self.ROI_SIZE)
+
+        disp_real = disp_score * 8 * ((self.s_x * chosen_scale) / self.ROI_SIZE)
+
         self.pos += disp_real
         
         return [int(self.pos[1] - self.target_sz[1]/2),
                 int(self.pos[0] - self.target_sz[0]/2),
-                self.target_sz[1], self.target_sz[0]]
+                int(self.target_sz[1]), int(self.target_sz[0])]
 
     
 def demo(model_path, image_folder_path):
