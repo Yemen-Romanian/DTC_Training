db03466a29b9f2b212bf1813d1128dd82ed428d7
Konstantin <kostya.kiev.ua@gmail.com>
2026-04-07 11:18:00 +0300

    Added module for calculating metrics

---
+++ src/evaluation/metrics.py
@@ -0,0 +1,132 @@
+import sys
+import math
+from itertools import product
+from pathlib import Path
+import pandas as pd
+
+
+def intersection_area(r1, r2):
+    """
+        r1, r2: tuples (x1, y1, x2, y2)
+    """
+    x1 = max(r1[0], r2[0])
+    y1 = max(r1[1], r2[1])
+    x2 = min(r1[2], r2[2])
+    y2 = min(r1[3], r2[3])
+
+    width = max(0, x2 - x1)
+    height = max(0, y2 - y1)
+
+    return width * height
+
+
+def area(r):
+    width = max(0, r[2] - r[0])
+    height = max(0, r[3] - r[1])
+    return width * height
+
+
+def union_area(r1, r2):
+    return area(r1) + area(r2) - intersection_area(r1, r2)
+
+
+def center_distance(r1, r2):
+    x1_1, y1_1, x2_1, y2_1 = r1
+    x1_2, y1_2, x2_2, y2_2 = r2
+    cx1 = (x1_1 + x2_1) / 2
+    cy1 = (y1_1 + y2_1) / 2
+    cx2 = (x1_2 + x2_2) / 2
+    cy2 = (y1_2 + y2_2) / 2
+    return math.hypot(cx2 - cx1, cy2 - cy1)
+
+
+def match_boxes(df1, df2):
+    tp = 0
+    m1 = m2 = set()
+    for (i, r1), (j, r2) in product(enumerate(df1.itertuples()), enumerate(df2.itertuples())):
+        if not (i in m1 or j in m2):
+            box1 = r1.x1, r1.y1, r1.x1 + r1.w, r1.y1 + r1.h
+            box2 = r2.x1, r2.y1, r2.x2, r2.y2
+
+            print('---------------------')
+            print('box1', box1, 'area', area(box1))
+            print('box2', box2, 'area', area(box2))
+            print('I/U', intersection_area(box1, box2) / union_area(box1, box2))
+            print('I/G', intersection_area(box1, box2) / area(box1))
+            print('dist', center_distance(box1, box2))
+            print('dist_norm', center_distance(box1, box2) / math.sqrt(area(box1)))
+
+            #if intersection_area(box1, box2) / union_area(box1, box2) > 0.5:
+            #if intersection_area(box1, box2) / area(box1) > 0.5:
+            if center_distance(box1, box2) / math.sqrt(area(box1)) < 1/3:
+                m1.add(i)
+                m2.add(j)
+                tp += 1
+
+    fp = len(df2) - tp
+    fn = len(df1) - tp
+    return tp, fp, fn
+
+
+def calculate_metrics(df1, df2):
+    """
+        Returns: (tp, fp, fn, precision, recall)
+    """
+    tp, fp, fn = 0, 0, 0
+    nframe = max(df1.iloc[-1].frame_id, df2.iloc[-1].frame_id)
+    df1 = df1.set_index("frame_id")
+    df2 = df2.set_index("frame_id")
+
+    for frame in range(1, nframe + 1):
+        df1_frame = df1.loc[[frame]] if frame in df1.index else df1.iloc[0:0]
+        df2_frame = df2.loc[[frame]] if frame in df2.index else df2.iloc[0:0]
+        tpi, fpi, fni = match_boxes(df1_frame, df2_frame)
+        tp += tpi
+        fp += fpi
+        fn += fni
+
+        #if frame == 20: break
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+
+    return tp, fp, fn, precision, recall
+
+
+def run(df1, df2):
+    metrics = pd.DataFrame(columns=["conf", "tp", "fp", "fn", "precision", "recall"])
+
+    for i in range(10, 11):
+        conf = i / 10
+        df2conf = df2[df2["conf"] >= conf]
+        tp, fp, fn, precision, recall = calculate_metrics(df1, df2conf)
+        new_row = {
+            "conf":      conf,
+            "tp":        tp,
+            "fp":        fp,
+            "fn":        fn,
+            "precision": precision,
+            "recall":    recall
+        }
+        metrics.loc[len(metrics)] = new_row
+
+    return metrics
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python metrics.py <ground_truth.csv> <tracking_results.csv>")
+        sys.exit(1)
+
+    path1 = sys.argv[1]
+    path2 = sys.argv[2]
+
+    df1 = pd.read_csv(path1, names=['frame_id', "track_id", "x1", 'y1', 'w', 'h', 'conf'])
+    df2 = pd.read_csv(path2)
+
+    metrics = run(df1, df2)
+    metrics.to_csv(Path(path2).with_suffix('.metrics.3.csv'), index=False, float_format="%.3f")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/evaluation/test_metrics.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from metrics import match_boxes, calculate_metrics
+
+def test_match_boxes1():
+    df1 = pd.DataFrame([{"x1": 1, "y1": 1, 'w': 10, 'h': 10}])
+    df2 = pd.DataFrame([{"x1": 3, "y1": 3, 'x2': 13, 'y2': 13}])
+
+    tp, fp, fn = match_boxes(df1, df2)
+    assert (tp, fp, fn) == (1,0,0)
+
+
+def test_match_boxes2():
+    df1 = pd.DataFrame([
+        {"x1": 1, "y1": 1, 'w': 10, 'h': 10},
+        {"x1": 100, "y1": 1, 'w': 10, 'h': 10},
+    ])
+    df2 = pd.DataFrame([
+        {"x1": 1, "y1": 1, 'x2': 10, 'y2': 10},
+        {"x1": 1, "y1": 100, 'x2': 10, 'y2': 110},
+    ])
+
+    tp, fp, fn = match_boxes(df1, df2)
+    assert (tp, fp, fn) == (1,1,1)
+
+
+def test_match_boxes3():
+    df1 = pd.DataFrame([
+        {"x1": 1, "y1": 1, 'w': 10, 'h': 10},
+        {"x1": 100, "y1": 1, 'w': 10, 'h': 10},
+    ])
+    df2 = pd.DataFrame()
+
+    tp, fp, fn = match_boxes(df1, df2)
+    assert (tp, fp, fn) == (0,0,2)
+
+
+def test_calculate_metrics():
+    df1 = pd.DataFrame([
+        {'frame_id': 1, "x1": 1, "y1": 1, 'w': 10, 'h': 10},
+        {'frame_id': 1, "x1": 100, "y1": 1, 'w': 10, 'h': 10},
+        {'frame_id': 10, "x1": 1, "y1": 1, 'w': 10, 'h': 10},
+        {'frame_id': 10, "x1": 100, "y1": 1, 'w': 10, 'h': 10},
+    ])
+    df2 = pd.DataFrame([
+        {'frame_id': 1, "x1": 1, "y1": 1, 'x2': 10, 'y2': 10},
+        {'frame_id': 1, "x1": 1, "y1": 100, 'x2': 10, 'y2': 110},
+        {'frame_id': 10, "x1": 1, "y1": 1, 'x2': 10, 'y2': 10},
+        {'frame_id': 10, "x1": 1, "y1": 100, 'x2': 10, 'y2': 110},
+    ])
+
+    tp, fp, fn, prec, recall = calculate_metrics(df1, df2)
+    assert (tp, fp, fn) == (2,2,2)
