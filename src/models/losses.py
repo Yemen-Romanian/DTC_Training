af53d010f8e8f1d77d18a94522d9a170e1af1021
Yevhen Romaniak <romanyak.yevhen@gmail.com>
2026-05-12 13:29:22 +0300

    Adopted balanced loss for gausiian ground truth label

--- src/models/losses.py
+++ src/models/losses.py
@@ -4,25 +4,26 @@ import torch.nn.functional as F
 
 
 class BalancedLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, pos_threshold=0.1):
         super(BalancedLoss, self).__init__()
+        self.pos_threshold = pos_threshold
 
     def forward(self, prediction, target):
         """
-        prediction: [B, 1, 17, 17] - network's logits
-        target: [B, 1, 17, 17] - binary gt mask
+        prediction: [B, 17, 17] - network's logits
+        target:     [B, 17, 17] - gt mask (binary or gaussian)
         """
-        pos_mask = (target == 1).float()
-        neg_mask = (target == 0).float()
+        pos_mask = (target >= self.pos_threshold).float()
+        neg_mask = (target <  self.pos_threshold).float()
 
         n_pos = pos_mask.sum()
         n_neg = neg_mask.sum()
 
         weight = torch.zeros_like(target)
         if n_pos > 0:
-            weight[target == 1] = 0.5 / n_pos
+            weight += pos_mask * (0.5 / n_pos)
         if n_neg > 0:
-            weight[target == 0] = 0.5 / n_neg
+            weight += neg_mask * (0.5 / n_neg)
 
         loss = F.binary_cross_entropy_with_logits(
             prediction, target, weight=weight, reduction='sum'
